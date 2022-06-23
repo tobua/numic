1df9ddddb41e30c609c67af857c95175522c4435
Matthias Giger <i@matthiasgiger.com>
2022-06-23 20:40:17 +0200

    feat(script): add lint and format script implementation and publish
    release-npm

--- script/lint.ts
+++ script/lint.ts
@@ -1,1 +1,9 @@
-export const lint = () => {}
+import { execSync } from 'child_process'
+import { log } from '../helper'
+
+export const lint = () => {
+  log('Linting')
+  execSync(`eslint . --ext .js,.ts,.tsx`, { stdio: 'inherit' })
+  log('Formatting')
+  execSync('prettier "{,!(android|ios)/**/}*.{ts,tsx}" --write', { stdio: 'inherit' })
+}
