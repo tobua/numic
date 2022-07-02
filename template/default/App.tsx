49e3eb8c6323e8fc7f0ce643797468d32abf6ea5
Matthias Giger <i@matthiasgiger.com>
2022-07-02 20:45:10 +0200

    fix(template): template improvements
    postinstall script will not run without name in package.json.

--- template/default/App.tsx
+++ template/default/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { SafeAreaView, StatusBar, StyleSheet, View, Text } from 'react-native'
+import { SafeAreaView, StatusBar, StyleSheet, View, Text, Image } from 'react-native'
+import logo from './logo.png'
 
 const styles = StyleSheet.create({
   safeArea: {
@@ -7,9 +8,20 @@ const styles = StyleSheet.create({
   },
   wrapper: {
     padding: 20,
+    alignItems: 'center',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginTop: 40,
+    marginBottom: 40,
   },
   text: {
     fontWeight: 'bold',
+    fontSize: 20,
+  },
+  green: {
+    color: '#3add85',
   },
 })
 
@@ -18,7 +30,10 @@ export function App() {
     <SafeAreaView style={styles.safeArea}>
       <StatusBar barStyle="dark-content" backgroundColor="#FFFFFF" />
       <View style={styles.wrapper}>
-        <Text style={styles.text}>Hello numic!</Text>
+        <Image style={styles.image} source={logo} />
+        <Text style={styles.text}>
+          Welcome to <Text style={styles.green}>numic</Text>!
+        </Text>
       </View>
     </SafeAreaView>
   )
