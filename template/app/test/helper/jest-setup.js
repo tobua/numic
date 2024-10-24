b9e4d9abeccbc78a699849318f92955782cc2d04
Matthias Giger <i@matthiasgiger.com>
2024-10-24 18:53:53 +0200

    fix(template): add Jest mock for native modules
    directly mockable NativeModules no longer available on new architecture.

---
+++ template/app/test/helper/jest-setup.js
@@ -0,0 +1,80 @@
+const { jest } = require('@jest/globals')
+
+// Mocks turbo modules not available in jest.
+jest.mock('react-native/Libraries/TurboModule/TurboModuleRegistry', () => {
+  const turboModuleRegistry = jest.requireActual(
+    'react-native/Libraries/TurboModule/TurboModuleRegistry'
+  )
+  return {
+    ...turboModuleRegistry,
+    getEnforcing: (name) => {
+      // List of TurboModules libraries to mock.
+      const modulesToMock = {
+        PlatformConstants: {
+          getConstants() {
+            return {}
+          },
+        },
+        SourceCode: {
+          getConstants() {
+            return {
+              scriptURL: null,
+            }
+          },
+        },
+        ImageLoader: {
+          getSize: jest.fn(() => Promise.resolve({ width: 320, height: 240 })),
+          prefetchImage: jest.fn(),
+        },
+        SettingsManager: {
+          getConstants() {
+            return {
+              settings: {
+                AppleLocale: 'en-us',
+                AppleLanguages: ['en-us'],
+              },
+            }
+          },
+        },
+        StatusBarManager: {
+          setColor: jest.fn(),
+          setStyle: jest.fn(),
+          setHidden: jest.fn(),
+          setNetworkActivityIndicatorVisible: jest.fn(),
+          setBackgroundColor: jest.fn(),
+          setTranslucent: jest.fn(),
+          getConstants: () => ({
+            HEIGHT: 42,
+          }),
+        },
+        DeviceInfo: {
+          getConstants() {
+            return {
+              Dimensions: {
+                window: {
+                  fontScale: 2,
+                  height: 1334,
+                  scale: 2,
+                  width: 750,
+                },
+                screen: {
+                  fontScale: 2,
+                  height: 1334,
+                  scale: 2,
+                  width: 750,
+                },
+              },
+            }
+          },
+        },
+        DevSettings: {},
+      }
+      if (Object.hasOwn(modulesToMock, name)) {
+        return modulesToMock[name]
+      } else {
+        console.warn(`Missing mock for native module "${name}" add in test/helper/jest-setup.js.`)
+      }
+      return turboModuleRegistry.getEnforcing(name)
+    },
+  }
+})
--- template/app/test/native.mock.ts
+++
@@ -1,8 +0,0 @@
-import { NativeModules } from 'react-native'
-
-// Mock locale depending on native language by os.
-NativeModules.SettingsManager = {
-  settings: {
-    AppleLocale: 'en_US',
-  },
-}
